f22dcafff850f06c5264b0ef39052dad693501cb
tugwellchristi <tugwellchristi@gmail.com>
2023-12-26 21:14:09 -0800

    adjusted sizes on shapes, generating logos, saved logos, added test files

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const { getInput } = require('./lib/input');
-const { Circle, Square, Triangle } = require('./lib/shapes');
+const shapes = require('./lib/shapes');
+const { generateSVG } = require('./lib/generator');
+
+async function create()  {
+    const input = await getInput();
+
+    const selectedShape = new shapes[input.shape]();
+    selectedShape.setColor(input.logoColor);
+    selectedShape.setText(input.letter);
+    selectedShape.setTextColor(input.textColor);
+
+    await generateSVG(selectedShape);
+}
+
+create();
 
